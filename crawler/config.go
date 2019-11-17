815e399e0a03cb0b6e20ab1bfe6d7b5894d89200
Casey Strouse <casey.strouse@gmail.com>
2019-11-16 22:20:56 -0700

    Fix copied mutex lock value in crawler assignment

--- crawler/config.go
+++ crawler/config.go
@@ -6,15 +6,13 @@ import (
 	"io/ioutil"
 )
 
-var defaultCrawler = Crawler{
-	Connections:     1,
-	MaxDepth:        0,
-	RobotsUserAgent: "Crawler",
-	WaitTime:        "100ms",
-}
-
 func FromJSON(in io.Reader) (*Crawler, error) {
-	config := defaultCrawler
+	config := Crawler{
+		Connections:     1,
+		MaxDepth:        0,
+		RobotsUserAgent: "Crawler",
+		WaitTime:        "100ms",
+	}
 
 	configJSON, err := ioutil.ReadAll(in)
 	if err != nil {
